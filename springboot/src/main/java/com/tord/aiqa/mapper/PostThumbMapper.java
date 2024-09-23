068ed5a4f5a01289a00ed55ff4fd9f09c0d16812
TordLei <tordlei1720@gmail.com>
2024-09-23 19:38:49 +0800

    初始化~

---
+++ springboot/src/main/java/com/tord/aiqa/mapper/PostThumbMapper.java
@@ -0,0 +1,17 @@
+package com.tord.aiqa.mapper;
+
+import com.tord.aiqa.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
---
+++ springboot/src/main/java/com/tord/aiqa/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.tord.aiqa.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.tord.aiqa.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
